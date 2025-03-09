847b7a37c72aa44139392e27c8321b36c68dbd2f
youssefhossam29 <yh5719731@gmail.com>
2025-03-09 05:43:14 +0200

    chnages in migrations files

--- database/migrations/2025_02_27_195317_create_profiles_table.php
+++ database/migrations/2025_02_27_195317_create_profiles_table.php
@@ -13,8 +13,7 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->longText('bio')->nullable();
 	        $table->string('city')->nullable();
 	        $table->Boolean('gender')->nullable();
--- database/migrations/2025_02_28_002439_create_notes_table.php
+++ database/migrations/2025_02_28_002439_create_notes_table.php
@@ -13,8 +13,7 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('title');
             $table->longText('content')->nullable();
             $table->string('image')->nullable();
