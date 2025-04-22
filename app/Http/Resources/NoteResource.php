5419238b86ca75d25203aecb6cb0928ca07c1bbb
youssefhossam29 <yh5719731@gmail.com>
2025-04-23 01:35:04 +0200

    Create Resources file for Note, and User to customize response

---
+++ app/Http/Resources/NoteResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NoteResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        //return parent::toArray($request);
+        return[
+            'slug' => $this->slug,
+            'user_id' => $this->user_id,
+            'user_name' => $this->user->name,
+            'title' => $this->title,
+            'content' => $this->content,
+            'image' => 'uploads/notes/' . $this->image,
+            'deleted_at' => ($this->deleted_at == null ) ? null: $this->deleted_at->format('d/m/y'),
+            'created_at' => $this->created_at->format('d/m/y'),
+            'updated_at' => $this->updated_at->format('d/m/y'),
+        ];
+    }
+}
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        //return parent::toArray($request);
+        if ($this->profile->gender === null) {
+            $gender = 'not selected';
+        } elseif ($this->profile->gender === 1) {
+            $gender = 'male';
+        } else {
+            $gender = 'female';
+        }
+
+        return[
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'image' => 'uploads/users/' .$this->profile->image,
+            'bio' => $this->profile->bio,
+            'city' => $this->profile->city,
+            'gender' => $gender,
+            'created_at' => $this->created_at->format('d/m/y'),
+            'updated_at' => $this->updated_at->format('d/m/y'),
+        ];    }
+}
