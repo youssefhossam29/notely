3b1fed7a4986d84e4f606921af0dc05a44eae6b9
youssefhossam29 <yh5719731@gmail.com>
2025-05-25 05:53:37 +0300

    Prevent Browser Back Button after Logout

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,9 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'prevent-back'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['prevent-back', 'auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
